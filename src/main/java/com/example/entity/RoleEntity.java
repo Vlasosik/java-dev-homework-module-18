e950bf4cd98ba3b788e12447f4ab85312000e9e8
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-24 22:10:40 +0200

    added role entity

---
+++ src/main/java/com/example/entity/RoleEntity.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import com.example.entity.enum_role.RoleName;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "role")
+public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+}
