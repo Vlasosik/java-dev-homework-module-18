d4d79acb77deb02f28d7a03b00d920e135db65d9
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-24 22:04:18 +0200

    changed the type of the first migration creation

---
+++ src/main/resources/db/migration/V1__create_note_users_or_role_db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE IF NOT EXISTS users (
+    id bigint auto_increment primary key,
+    login varchar(100) unique not null,
+    password varchar(64) not null
+);
+CREATE TABLE IF NOT EXISTS note (
+    id bigint auto_increment primary key,
+    user_id bigint,
+    title varchar(100) not null,
+    context varchar(100) not null,
+    foreign key (user_id) references users(id)
+);
+CREATE TABLE role (
+    id bigint auto_increment primary key,
+    name enum ('ROLE_USER', 'ROLE_ADMIN') not null
+);
