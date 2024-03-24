fe390badef52136e861de603e4f2bd76920aedb7
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-24 22:11:28 +0200

    added security constant

---
+++ src/main/java/com/example/security/SecurityConstant.java
@@ -0,0 +1,8 @@
+package com.example.security;
+
+public class SecurityConstant {
+    private SecurityConstant(){
+
+    }
+    public static final String SECRET_KEY = "SECRETKEYSECRETKEYSECRETKEYSECRETKEYSECRETKEYSECRETKEY";
+}
