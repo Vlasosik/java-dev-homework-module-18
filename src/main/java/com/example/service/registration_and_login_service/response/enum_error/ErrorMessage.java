c3d04b173585678e4ea90a5a21cd94f48a1a450f
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-24 22:10:00 +0200

    added ErrorMessage

---
+++ src/main/java/com/example/service/registration_and_login_service/response/enum_error/ErrorMessage.java
@@ -0,0 +1,9 @@
+package com.example.service.registration_and_login_service.response.enum_error;
+
+public enum ErrorMessage {
+    OK,
+    USER_ALREADY_EXISTS,
+    INVALID_LOGIN,
+    INVALID_PASSWORD,
+    USER_DOES_NOT_EXIST
+}
