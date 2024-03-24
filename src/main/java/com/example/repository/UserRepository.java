3773ebcd2e64289f1c89f36855a69ad12d7b2452
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-24 22:06:31 +0200

    added User repository

---
+++ src/main/java/com/example/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.repository;
+
+import com.example.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByLogin(String loginName);
+}
