66a639211a74b125c1428bf3b3f0d94702dbdc25
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-24 22:07:58 +0200

    added UserEntity

---
+++ src/main/java/com/example/entity/UserEntity.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+}
+
