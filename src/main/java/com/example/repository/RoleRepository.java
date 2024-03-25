091384bb9516cd7b3af141ca25da5e5bbd929c1c
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-25 20:30:08 +0200

    added method find by rolename

--- src/main/java/com/example/repository/RoleRepository.java
+++ src/main/java/com/example/repository/RoleRepository.java
@@ -1,9 +1,13 @@
 package com.example.repository;
 
 import com.example.entity.RoleEntity;
+import com.example.entity.enum_role.RoleName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RoleRepository extends JpaRepository<RoleEntity, Long> {
+    Optional<RoleEntity>findByRoleName(RoleName roleName);
 }
