927e68fcda46f763ac9360c40d24a042c5b8f85e
Vlasosik <128188585+Vlasosik@users.noreply.github.com>
2024-03-25 20:30:41 +0200

    modified the first migration

--- src/main/resources/db/migration/V1__create_note_users_or_role_db.sql
+++ src/main/resources/db/migration/V1__create_note_users_or_role_db.sql
@@ -1,7 +1,13 @@
+CREATE TABLE IF NOT EXISTS role (
+    id bigint auto_increment primary key,
+    name enum ('ROLE_USER', 'ROLE_ADMIN') not null
+);
 CREATE TABLE IF NOT EXISTS users (
     id bigint auto_increment primary key,
+    role_id bigint,
     login varchar(100) unique not null,
-    password varchar(64) not null
+    password varchar(64) not null,
+    foreign key(role_id) references role(id)
 );
 CREATE TABLE IF NOT EXISTS notes (
     id bigint auto_increment primary key,
@@ -9,8 +15,4 @@ CREATE TABLE IF NOT EXISTS notes (
     title varchar(100) not null,
     context varchar(100) not null,
     foreign key (user_id) references users(id)
-);
-CREATE TABLE role (
-    id bigint auto_increment primary key,
-    name enum ('ROLE_USER', 'ROLE_ADMIN') not null
 );
